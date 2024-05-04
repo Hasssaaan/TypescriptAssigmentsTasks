e4d784f51a127ac7d65c6d5f0a6771da0cde2403
Hassaan <jahangirhassan78@gmail.com>
2024-05-04 05:01:32 +0500

    Assignment5

--- assignmnet5(35Questions)/question9.ts
+++ assignmnet5(35Questions)/question9.ts
@@ -1,6 +1,6 @@
 //Create a script that prompts the user to enter their age and then logs whether they are a minor (under 18) or an adult.
-let age=19;
-if(age>=18){
+let agee=19;
+if(agee>=18){
     console.log("Person is adult");
 }
 else{
